30fdc7707511f640529dbfdfd929e188b0089bca
workceostewart-lang <workceostewart@gmail.com>
2026-07-29 20:47:17 -0700

    Support nested FantomZone launch path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "./",
   cacheDir: ".vite-cache",
   build: {
     target: "es2022",
-    sourcemap: true,
+    sourcemap: false,
   },
   server: {
     host: "127.0.0.1",
